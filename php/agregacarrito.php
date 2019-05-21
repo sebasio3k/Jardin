2c60d9ceb9bf1185ee01f74c719ae953e60f3d41
sebasio3k <bastianhdezo@gmail.com>
2019-05-21 14:43:11 -0500

    listo

--- php/agregacarrito.php
+++ php/agregacarrito.php
@@ -2,91 +2,6 @@
     session_start();
     $codi=$_POST['codi'];
     $_SESSION['can'] = $_POST['can'];
-<<<<<<< HEAD
-    $salida = "";
-
-    $longitud=count($_SESSION['carrito']);
-    $fila=$_SESSION['carrito'];
-    // if($resultado -> num_rows > 0){
-        $salida.= "<div class='card-deck'>
-                    ";
-        $count = 0;
-        $count2=0;
-        // while($fila=$resultado->fetch_assoc()){
-            // $count2++;
-            for ($o=0; $o<$longitud; $o++){
-                $count2++;
-            $salida.= "
-
-                            <!-- Tarjeta de producto -->
-                            <div class='card' style='width: 18rem;'>
-                                <img src='./img/prod/".$fila[$o]['idproducto'].".jpg'  class='card-img-top img-fluid' alt='".$fila[$o]['descripcion']."'>
-                                <div class='card-body '>
-                                    <h5 class='card-title blurw'>".$fila[$o]['descripcion']."</h5>
-                                    <p class='card-text blurw text-justify'>".$fila[$o]['precio'].".</p>
-                                    <button type='button' href='#' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal' onmouseover='enviar(".$count2.")'>Me interesa...</button>
-                                </div>
-                            </div>
-
-                    ";
-                $count++;
-                for ($i = $count; $i == 4; $i++) {
-                    // cuando sean 4
-                    $salida.="
-                    </div>
-                    <br>
-                    <div class='card-deck'>
-                    ";
-                    // reinicia contador
-                    $count=0;
-                    $i=0;
-                }
-        }
-        $salida.="<!-- ultimo -->
-                </div>
-                ";
-
-
-    // else {
-        // $salida.="No hay datos :c";
-    // }
-    echo $salida;
-    
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        //     if(!isset($_SESSION['carrito'])){
-        //         $listac=array(
-        //             'idproducto'=>$codi,
-        //             // 'idcategoria'=>$fila['idcategoria'],
-        //             'descripcion'=>$_SESSION['productos'][$codi]['descripcion'],
-        //             'precio'=>$_SESSION['productos'][$codi]['precio']
-        //             // 'ruta'=>$fila['ruta']
-        //             // 'stock'=>$id,
-        //         );
-        //         $_SESSION['carrito'][0]=$listac;
-        //     }
-        //     else{
-        //         $numeroPrdo=count($_SESSION['carrito']);
-        //         $listac=array(
-        //             'idproducto'=>$codi,
-        //             // 'idcategoria'=>$fila['idcategoria'],
-        //             'descripcion'=>$_SESSION['productos'][$codi]['descripcion'],
-        //             'precio'=>$_SESSION['productos'][$codi]['precio']
-        //             // 'ruta'=>$fila['ruta']
-        //             // 'stock'=>$id,
-        //         );
-        //         $_SESSION['carrito'][$numeroPrdo]=$listac;
-        //     }
-        //     $_SESSION['cuantos']=count($_SESSION['carrito']);
-        //    echo  print_r($_SESSION['carrito'],true);
-=======
     // $cantidad = =$_POST['can'];
     $salida = "";
 
@@ -122,6 +37,5 @@
     }
     $_SESSION['cuantos']=count($_SESSION['carrito']);
     echo  print_r($_SESSION['carrito'],true);
->>>>>>> a36e8f9202b43b72913cd7c027fbca340f14e9ac
 
 ?>
--- php/consultarCarrito.php
+++ php/consultarCarrito.php
@@ -1,59 +1,4 @@
 <?php
-<<<<<<< HEAD
-    // include("conexion.php");
-    // $id = $_POST['buscar'];
-
-    session_start();
-
-    $codi=$_REQUEST['cod'];
-    // $can = $_SESSION['can'];
-    $_SESSION['codigo']=$codi;
-    $listap = $_SESSION['productos'];
-    $salida = "";
-    $longitud=count($_SESSION['productos']);
-    $nombre=$_SESSION['productos'][$codi]['descripcion'];
-    $precio=$_SESSION['productos'][$codi]['precio'];
-
-    $mysqli = new mysqli("localhost", "Sebastian", "ifuseekamy", "jardinabuela");
-    $salida = "";
-    $query = "SELECT * FROM cursos ORDER BY idcurso";
-    $q="";
-    if(isset($_POST['consulta'])){
-        $q = $mysqli->real_escape_string($_POST['consulta']);
-        $query = "SELECT idcurso,nombrecurso,descripcion,precio,fecha FROM cursos WHERE idcurso LIKE '%".$q."%' OR nombrecurso LIKE '%".$q."%' OR precio LIKE '%".$q."%' ";
-    }
-
-    $resultado = $mysqli->query($query);
-
-    // if ($resultado == ""){
-
-    // }
-    // else {
-    if($q==""){
-
-    }
-    else{
-        if($resultado -> num_rows > 0){
-            $salida.= "<table class='table bgblanco textcolorb table-responsive table-striped table-bordered table-sm'>
-                            <thead class='thead-dark'>
-                                <tr>
-                                    <th scope='col'>ID</th>
-                                    <th scope='col'>Nombre Curso</th>
-                                    <th scope='col'>Descripción</th>
-                                    <th scope='col'>Precio</th>
-                                </tr>
-                            </thead>
-                            <tbody>";
-            while($fila=$resultado->fetch_assoc()){
-                $salida.= "<tr>
-                            <td>".$fila['idcurso']."</td>
-                            <td>".$fila['nombrecurso']."</td>
-                            <td>".$fila['descripcion']."</td>
-                            <td>".$fila['precio']."</td>
-                            </tr>";
-            }
-            $salida.="</tbody></table>";
-=======
     session_start();
     $salida = "";
     if(!isset($_SESSION['carrito'])){
@@ -79,7 +24,6 @@
                                 <tbody>";
                     for ($i = 0; $i < $longitud; $i++) {
                         $salida.= "<tr>
-                                        
                                         <td scope='row'>".$fila[$i]['descripcion']."</td>
                                         <td>".$fila[$i]['precio']."</td>
                                         <td>".$fila[$i]['cant']."</td>
@@ -89,17 +33,9 @@
             // }
             $salida.="</tbody></table>";
             echo $salida;
->>>>>>> a36e8f9202b43b72913cd7c027fbca340f14e9ac
         }
         else {
             $salida.="No hay datos :c";
         }
-<<<<<<< HEAD
-    // }
     }
-    echo $salida;
-    $mysqli->close();
-=======
-    }
->>>>>>> a36e8f9202b43b72913cd7c027fbca340f14e9ac
 ?>
